57604c8cb973f68f873b39633d54f97c7b2eaea2
unknown <amorzaki05@gmail.com>
2023-08-06 13:57:31 +0100

    commit into light mode

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 const BlogPost = () => {
   return (
-    <div>BlogPost</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
+            beatae, a suscipit eos. Animi quibusdam cum omnis officiis
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Amor Zaki</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae
+          dolor, optio voluptatibus magnam iure esse tempora beatae, a suscipit
+          eos. Animi quibusdam cum omnis officiis voluptatum quo ea eveniet?
+     
+          <br/>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae
+          dolor, optio voluptatibus magnam iure esse tempora beatae, a suscipit
+          <br/>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae
+          dolor, optio voluptatibus magnam iure esse tempora beatae, a suscipit
+          eos. Animi quibusdam cum omnis officiis voluptatum quo ea eveniet?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. - Creative
+          Illustrations
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default BlogPost
+export default BlogPost;
