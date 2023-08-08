3af40cb9e23279ab99d640c6ae32ecb055110a45
unknown <amorzaki05@gmail.com>
2023-08-08 14:36:20 +0100

    commit adding mongo database

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,36 +1,42 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
-const BlogPost = () => {
+import { notFound } from "next/navigation";
+async function getData(id) {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return notFound();
+  }
+
+  return res.json();
+}
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
-            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
-            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
-            beatae, a suscipit eos. Animi quibusdam cum omnis officiis
+            {data.desc}
           </p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              src={data.img}
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>Amor Zaki</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            src={data.img}
             alt=""
             fill={true}
             className={styles.image}
@@ -39,19 +45,7 @@ const BlogPost = () => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae
-          dolor, optio voluptatibus magnam iure esse tempora beatae, a suscipit
-          eos. Animi quibusdam cum omnis officiis voluptatum quo ea eveniet?
-     
-          <br/>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae
-          dolor, optio voluptatibus magnam iure esse tempora beatae, a suscipit
-          <br/>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae
-          dolor, optio voluptatibus magnam iure esse tempora beatae, a suscipit
-          eos. Animi quibusdam cum omnis officiis voluptatum quo ea eveniet?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. - Creative
-          Illustrations
+         {data.content}
         </p>
       </div>
     </div>
